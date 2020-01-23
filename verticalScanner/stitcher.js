8dd783c6f1505ee08828e32984db36cfecacb881
Ezekiel Aquino <ezekielaquino@gmail.com>
2020-01-23 15:07:56 +0100

    🦄 Refactor stitcher

--- verticalScanner/stitcher.js
+++ verticalScanner/stitcher.js
@@ -1,19 +1,15 @@
-const Jimp = require('jimp2');
-
-//https://www.npmjs.com/package/jimp2
-
-Jimp.read("./print/2310298.png", (err, larger) => {
-  larger.resize(480, 1200);
-
-  Jimp.read("./print/2310298.png", (err, orig) => {
+Promise.all([
+  Jimp.read(`./print/${sourceFile}.png`),
+  Jimp.read(`./print/${sourceFile}.png`),
+  Jimp.read(`./print/${sourceFile}.png`),
+])
+  .then(([ larger, orig, flip ]) => {
+    larger.resize(480, 1200);
     larger.composite(orig, 0, 0);
+    flip.mirror(false, true);
 
-    Jimp.read("./print/2310298.png", (err, flip) => {
-      flip.mirror(false, true);
-
-      larger
-        .composite(flip, 0, 600)
-        .write(`./finalprint/${someId}.png`);
-    });
+    larger
+      .composite(flip, 0, 600)
+      .grayscale(100)
+      .write(`./finalprint/${someId}.png`);
   });
-});
