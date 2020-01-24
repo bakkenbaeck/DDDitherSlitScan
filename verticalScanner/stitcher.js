052defa0a375dd7aab7acdeb3e9ebbf4e7ca0539
Ezekiel Aquino <ezekielaquino@gmail.com>
2020-01-24 15:32:46 +0100

    Update stitcher.js

--- verticalScanner/stitcher.js
+++ verticalScanner/stitcher.js
@@ -1,19 +1,21 @@
 const Jimp = require('jimp2');
 
 //https://www.npmjs.com/package/jimp2
-
-Promise.all([
-  Jimp.read(`./print/${sourceFile}.png`),
-  Jimp.read(`./print/${sourceFile}.png`),
-  Jimp.read(`./print/${sourceFile}.png`),
-])
-  .then(([ larger, orig, flip ]) => {
+// We're chaining this because jimp2's promises are not behaving as expected
+new Promise(resolve => {
+  Jimp.read(`./print/test.jpg`, (err, larger) => {
     larger.resize(480, 1200);
-    larger.composite(orig, 0, 0);
-    flip.mirror(false, true);
-
-    larger
-      .composite(flip, 0, 600)
-      .grayscale(100)
-      .write(`./finalprint/${someId}.png`);
+    Jimp.read(`./print/test.jpg`, (err, orig) => {
+      larger.composite(orig, 0, 0);
+      Jimp.read(`./print/test.jpg`, (err, flip) => {
+        flip.mirror(false, true);
+        larger
+          .composite(flip, 0, 600)
+          .write(`./finalprint/final.png`, () => {
+            resolve();
+            console.log('test');
+          });
+      });
+    });
   });
+});
